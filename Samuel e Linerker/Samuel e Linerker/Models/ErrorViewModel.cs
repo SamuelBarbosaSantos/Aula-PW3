d0034bbd04047162c40fbc8e14a74ff7ef6e5fcb
unknown <3mtec@e041edu.local>
2026-03-06 10:36:00 -0300

    aula dia 06

---
+++ Samuel e Linerker/Samuel e Linerker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Samuel_e_Linerker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Samuel e Linerker/Samuel e Linerker/Repository/Contract/IUsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using Samuel_e_Linerker.Models;
+
+namespace Samuel_e_Linerker.Repository.Contract
+{
+    public interface IUsuarioRepository
+    {
+        //CRUD
+        IEnumerable <Usuario> ObterTododsUsarios();
+
+        void Cadastrar(Usuario usuario);
+
+        void Atualizar(Usuario usuario);
+
+        Usuario ObterUsuario(int id);
+
+        void Excluir(int id);
+
+
+
+    }
+}
